6e0bd023f22a1a1b674d6fedd3ebf127b63dcdd6
Gerardo Felmer <lalote@gmail.com>
2019-02-13 17:40:33 -0300

    entendiendo algunas propiedades de css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,39 @@
 <template>
-  <v-layout text-xs-center wrap>
-    <v-flex>
-      <v-flex xs12>
-        <v-img :src="require('../assets/logo2.png')" contain class="logo"></v-img>
-      </v-flex>
-      <v-layout class="contenedor">
-        <div class="titulo_principal">
-          <h1>ENTRENA INTELIGENTE</h1>
-        </div>
-
-        <div class="titulo_principal">
-          <h2>CUANTIFICA TU PROGRESO</h2>
-        </div>
-
-        <v-flex xs12 class="poligono_arriba"></v-flex>
-
-        <!-- <div class="poligono_arriba"></div> -->
-        <!--       <v-flex xs12 class="poligono_arriba" row>
-        -->
-        <!-- <v-img :src="require('../assets/flecha-roja-arriba.png')"  ></v-img> -->
-        <!-- <div class="poligono_arriba"></div> -->
-        <!--  </v-flex> -->
-        <v-flex xs12 class="imagen_danny">
-          <v-img :src="require('../assets/danny-face.png')" class="imagen_danny"></v-img>
-        </v-flex>
-
-        <v-flex xs12 class="imagen_fondo">
-          <v-img :src="require('../assets/imagen_fondo.png')" class="imagen_fondo"></v-img>
-        </v-flex>
-        <!-- margin bottom mb-4 -->
-        <v-flex xs12 class="poligono_abajo">
-          <!-- <v-img :src="require('../assets/flecha-roja-abajo.png')"  ></v-img> -->
-          <!-- <div class="poligono_abajo"></div> -->
-        
-      </v-flex>
-      </v-layout>
+  <section class="contenedor">
+    <v-flex xs12 class="logo">
+      <v-img :src="require('../assets/logo2.png')" contain></v-img>
     </v-flex>
-  </v-layout>
+
+
+      <v-flex xs12 class="poligono_arriba"></v-flex>
+
+      <div class="titulo_principal">
+        <h1>ENTRENA INTELIGENTE</h1>
+      </div>
+
+      <div class="titulo_principal">
+        <h2>CUANTIFICA TU PROGRESO</h2>
+      </div>
+
+      <v-flex xs12 class="imagen_danny">
+        <v-img :src="require('../assets/danny-face.png')" class="imagen_danny"></v-img>
+      </v-flex>
+    
+    <!-- <div class="poligono_arriba"></div> -->
+    <!--       <v-flex xs12 class="poligono_arriba" row>
+    -->
+    <!-- <v-img :src="require('../assets/flecha-roja-arriba.png')"  ></v-img> -->
+    <!-- <div class="poligono_arriba"></div> -->
+    <!--  </v-flex> -->
+    <v-flex xs12 fill class="imagen_fondo">
+      <v-img :src="require('../assets/imagen_fondo.png')" class="imagen_fondo"></v-img>
+    </v-flex>
+    <!-- margin bottom mb-4 -->
+    <v-flex xs12 class="poligono_abajo">
+      <!-- <v-img :src="require('../assets/flecha-roja-abajo.png')"  ></v-img> -->
+      <!-- <div class="poligono_abajo"></div> -->
+    </v-flex>
+  </section>
 </template>
 
 <script>
@@ -48,76 +45,100 @@ export default {
 <style>
 .contenedor {
   margin-top: 10px;
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 
 .logo {
-  float: left;
-  padding-left: 100px;
-  padding-right: 60px;
-  margin-left: 20px;
-  height: 104px;
+  position: absolute;
 }
 
 .poligono_arriba {
   -webkit-clip-path: polygon(
-    0% 22%,
-    0% 23%,
-    36% 13%,
-    52% 24%,
-    100% 15%,
-    100% 9%,
-    54% 21%,
-    37% 11%
+    37% 12%,
+    54% 74%,
+    100% 2%,
+    100% 37%,
+    52% 94%,
+    36% 29%,
+    0% 91%,
+    0% 80%
   );
   clip-path: polygon(
-    0% 22%,
-    0% 23%,
-    36% 13%,
-    52% 24%,
-    100% 15%,
-    100% 9%,
-    54% 21%,
-    37% 11%
+    37% 12%,
+    54% 74%,
+    100% 2%,
+    100% 37%,
+    52% 94%,
+    36% 29%,
+    0% 91%,
+    0% 80%
   );
-  height: 720px;
+  height: 120px;
+  width: 100%;
   background: red;
   position: relative;
   z-index: 2;
+  top: 92px;
+}
+.poligono_abajo {
+  -webkit-clip-path: polygon(
+    0% 62%,
+    46% 3%,
+    62% 71%,
+    100% 3%,
+    100% 12%,
+    61% 88%,
+    45% 25%,
+    0 95%
+  );
+  clip-path: polygon(
+    0% 62%,
+    46% 3%,
+    62% 71%,
+    100% 3%,
+    100% 12%,
+    61% 88%,
+    45% 25%,
+    0 95%
+  );
+  height: 118px;
+  background: red;
+  position: relative;
+  top: -92px;
 }
 .imagen_fondo {
-  /*   -webkit-clip-path: polygon(
-    0 23%,
-    36% 13%,
-    52% 24%,
-    100% 14%,
-    100% 74%,
-    62% 85%,
-    47% 74%,
-    0 83%
-  ); */
+  -webkit-clip-path: polygon(
+    0 15%,
+    36% 1%,
+    52% 16%,
+    100% 3%,
+    100% 85%,
+    62% 99%,
+    47% 84%,
+    0% 97%
+  );
   clip-path: polygon(
-    0 23%,
-    36% 13%,
-    52% 24%,
-    100% 14%,
-    100% 74%,
-    62% 85%,
-    47% 74%,
-    0 83%
+    0 15%,
+    36% 1%,
+    52% 16%,
+    100% 3%,
+    100% 85%,
+    62% 99%,
+    47% 84%,
+    0% 97%
   );
 
-  position: absolute;
-  top: 52px;
-  width: 100%;
-  height: 720px;
+  height: 521px;
   z-index: 2;
+  width: 100%;
+  object-fit: fill;
 }
 .imagen_danny {
-  position: absolute;
-  top: 28px;
-  left: 110px;
-  width: 100%;
-  height: 1460px;
+  top : -50px;
+
+  position: relative;
   z-index: 99999;
 }
 .titulo_principal {
@@ -136,32 +157,6 @@ export default {
   clear: both;
 }
 .titulo_principal h2 {
-}
-
-.poligono_abajo {
-  -webkit-clip-path: polygon(
-    0 88%,
-    45% 77%,
-    61% 88%,
-    100% 75%,
-    100% 74%,
-    63% 84%,
-    47% 73%
-  );
-  clip-path: polygon(
-    0 88%,
-    45% 77%,
-    61% 88%,
-    100% 75%,
-    100% 74%,
-    63% 84%,
-    47% 73%
-  );
-  position: absolute;
-  height: 720px;
-  width: 100%;
-  background: red;
-  top: 63px;
 }
 
 .triangulo_blanco_abajo_izquierda {
